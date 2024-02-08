c8eb24931766b9d7025afb7c91e39039a5fa5662
Ziyodulla-Abdukarimov <ZiyodullaDeveloper@gmail.com>
2024-02-08 15:35:47 +0500

    firs

---
+++ app/src/main/java/com/example/deliveryapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.deliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.deliveryapp.databinding.ActivityMainBinding;
+import com.google.android.material.badge.BadgeDrawable;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        BadgeDrawable badgeDrawable = binding.bottomNavigationView.getOrCreateBadge(R.id.likes);
+        badgeDrawable.setVisible(true);
+        badgeDrawable.setNumber(3);
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.menu) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.likes) {
+                replaceFragment(new LikesFragment());
+            } else if (item.getItemId() == R.id.cart) {
+                replaceFragment(new CartFragment());
+            } else if (item.getItemId() == R.id.user) {
+                replaceFragment(new UserFragment());
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
